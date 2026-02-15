533fcb573500cccf795e6720cd04a523a4283fac
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-02-16 00:03:18 +0100

    :sparkles:  Added retry and backoff configuration for bullMQ Queue

--- backend/queue/index.js
+++ backend/queue/index.js
@@ -1,6 +1,16 @@
 const { Queue } = require('bullmq');
 const connection = require('./connection');
 
-const logQueue = new Queue('log-queue', { connection });
+const logQueue = new Queue('log-queue',
+    { connection },
+    {
+        defaultJobOptions: {
+            attempts: 3,
+            backoff: {
+                type: 'exponential',
+                delay: 1000,
+            },
+        }
+    });
 
 module.exports = logQueue;
