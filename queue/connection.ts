018fe29eeae3112b6a44075bbc2e1a6324022eba
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-02-18 09:57:25 +0100

    :zap: Fix not working worker

--- queue/connection.ts
+++ queue/connection.ts
@@ -1,8 +1,9 @@
-import { Redis } from 'ioredis';
+import IORedis from 'ioredis';
 
-const connection = new Redis({
+const connection = new IORedis({
     host: process.env.REDIS_HOST || 'localhost',
     port: Number(process.env.REDIS_PORT) || 6379,
+    maxRetriesPerRequest: null,
 });
 
 export default connection;
